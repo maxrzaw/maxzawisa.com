2e3a5cd1fb3f104cc2fa63ba75fe2f7ae4ecc8e5
Max Zawisa <maxrzaw@umich.edu>
2022-03-20 22:52:27 -0400

    some tic tac toe stuff

---
+++ tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,12 @@
+const values = [' ', 'X', 'O'];
+
+
+var board = ['', '', '', '', '', '', '', '', ''];
+var turn = 0;
+
+function resetBoard() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    turn = 0;
+}
+
+alert(board.length);
